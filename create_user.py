c26aa81468df86c564f6327de93791ae98c8bc26
gezeebeezee <57654189+gezeebeezee@users.noreply.github.com>
2025-07-05 01:41:53 -0700

    Began refactoring code to accommodate 'roles' (i.e. admin, counselor, team leader).
    TODO: fix edit_camper.html to match user permissions.

--- create_user.py
+++ create_user.py
@@ -1,26 +1,18 @@
-# from app import create_app, db
-# from app.models import User
-# from app.auth_utils import hash_password
-
-# app = create_app()
-# with app.app_context():
-#     db.create_all()
-
-#     # Create a user
-#     new_user = User(username="kle", password_hash="trickypassword", team_number=None)
-#     db.session.add(new_user)
-#     db.session.commit()
-
-#     print("User created successfully.")
-
 from app import create_app, db
 from app.models import User
 
 app = create_app()
 with app.app_context():
-    admin_user = User(username='admin')
-    admin_user.password = 'trickypassword'
-    admin_user.team_number = None  # Gives access to all teams
-    db.session.add(admin_user)
-    db.session.commit()
-    print("Admin user created.")
+    # Check if admin user already exists
+    if not User.query.filter_by(username='admin').first():
+        admin_user = User(
+            username='admin',
+            team_number=None,  # Admins don't need a team
+            role='admin'
+        )
+        admin_user.password = 'trickypassword'  # This sets the hashed password
+        db.session.add(admin_user)
+        db.session.commit()
+        print("Admin user created.")
+    else:
+        print("Admin user already exists.")
