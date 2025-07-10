488b4581b1457a0a9d2580a5ad75d55741932a22
gezeebeezee <57654189+gezeebeezee@users.noreply.github.com>
2025-07-09 17:06:51 -0700

    Updated init_db.py to create admin user and utilize environment variables for admin password.

--- init_db.py
+++ init_db.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app, db
 from app.models import User
 
@@ -5,10 +6,24 @@ app = create_app()
 
 with app.app_context():
     inspector = db.inspect(db.engine)
-    
-    # Check if the 'user' table already exists
+
+    # Step 1: Initialize DB if tables do not exist
     if not inspector.has_table('user'):
         db.create_all()
         print("✅ Database initialized.")
     else:
         print("ℹ️ Database already exists. Skipping initialization.")
+
+    # Step 2: Create admin user if not found
+    admin_username = 'admin'
+    admin_exists = User.query.filter_by(username=admin_username).first()
+
+    if not admin_exists:
+        admin_password = os.getenv('ADMIN_PASSWORD', 'trickypassword')
+        admin = User(username=admin_username, role='admin')
+        admin.password = admin_password
+        db.session.add(admin)
+        db.session.commit()
+        print("✅ Admin user created.")
+    else:
+        print("ℹ️ Admin user already exists.")
